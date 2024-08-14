9c700273fefb82b55c301346147dc5f2c92cf218
shivam <shivam.unique1@gmail.com>
2024-08-14 20:22:46 +0530

    push frontend code

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,18 +1,28 @@
-import React, { createContext, useState } from "react";
-import all_product from "../Components/Assets/all_product";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < all_product.length; index++) {
-    cart[all_product[index].id] = 0;
+  for (let index = 0; index < 300+1; index++) {
+    cart[index] = 0;
   }
   return cart;
 };
 
 const ShopContextProvider = (props) => {
+
+  const [all_product,setAll_product] = useState([])
+
   const [cartItems, setCartItems] = useState(getDefaultCart);
+
+  useEffect(()=>{
+
+    fetch('http://localhost:4000/allproducts')
+    .then((responce)=>responce.json())
+    .then((data)=>setAll_product(data))
+
+  },[])
 
   const addToCart = (itemId) => {
     setCartItems((prev) => {
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,24 +1,118 @@
-import React from 'react'
-import './css/LoginSignup.css'
+import React, { useState } from 'react';
+import './css/LoginSignup.css';
+
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = async () => {
+    console.log("Login Function Executed", formData);
+    let responseData;
+
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.error || "Login failed");
+    }
+  };
+
+  const signup = async () => {
+    console.log("Sign Up Function Executed", formData);
+    let responseData;
+
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.error || "Signup failed");
+    }
+
+  };
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+          {state === "Sign Up" && (
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="Your Name"
+            />
+          )}
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Email Address"
+          />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Password"
+          />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <button onClick={() => { state === "Login" ? login() : signup(); }}>
+          Continue
+        </button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account? <span onClick={() => setState("Login")}>Login here</span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account? <span onClick={() => setState("Sign Up")}>Click here</span>
+          </p>
+        )}
+
         <div className="loginsignup-agree">
-          <input type="checkbox" name='' id='' />
-          <p>By continuing, i agree to the terms of use & privacy policy</p>
+          <input type="checkbox" />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
